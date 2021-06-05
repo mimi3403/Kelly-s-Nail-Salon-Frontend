f5a8b2e1a8e23ad1a4907aa7ac17175ec0573a11
mimi3403 <miimiixo@gmail.com>
2021-06-05 19:27:01 -0400

    adds appointment form

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             )}
           />
           <Route
-            page='/contactus'
+            path='/contactus'
             render={() => (
               <ContactUsPage />
             )}
--- src/pages/ContactUsPage/ContactUsPage.js
+++ src/pages/ContactUsPage/ContactUsPage.js
@@ -1,12 +1,14 @@
 import NavBar from '../../components/NavBar/NavBar';
 import Footer from '../../components/Footer/Footer';
 import ContactUs from '../../components/ContactUs/ContactUs';
+import Appointment from '../../components/Appointment/Appointment';
 
 const ContactUsPage = () => {
   return (
     <div className="App">
       <NavBar />
       <ContactUs />
+      <Appointment />
       <Footer />
     </div>
   )
